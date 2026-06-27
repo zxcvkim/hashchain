8af3740f5a9c0622a1441ced43210abe6cbac287
zxcvkim <190175095+asdfkim@users.noreply.github.com>
2026-06-27 23:51:26 +0900

    🎉 Assemble!!! (`main.rs`)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use std::{thread, time::Duration};
+
+use hashchain_rs::{Block, HashChain};
+
+fn main() {
+    // let bytes = todo!();
+    // let mut chain = HashChain::from_bytes(bytes);
+
+    let mut chain = HashChain::new();
+    println!("{:?}", chain.to_bytes());
+
+    for i in 1..=5 {
+        thread::sleep(Duration::from_secs(1));
+
+        let prev_hash = chain.latest_block().unwrap().hash();
+        let content = format!("{}-block", i);
+        let new_block = Block::new(prev_hash, content);
+
+        chain.add_block(new_block);
+    }
+
+    println!("{:#?}", chain);
+    println!("{:?}", chain.to_bytes());
+}
