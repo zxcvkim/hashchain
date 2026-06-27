4353211d30d2577f3b4d460371fdaaa2d91a7470
zxcvkim <190175095+asdfkim@users.noreply.github.com>
2026-06-27 23:02:55 +0900

    ✨ Add [`Block`, `HashChain`] structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 pub mod crypto;
+pub mod types;
 pub mod util;
 
 pub use crypto::Hash;
+pub use types::Block;
+pub use types::HashChain;
---
+++ src/types.rs
@@ -0,0 +1,55 @@
+use bincode::{Encode, config};
+
+use crate::{crypto::Hash, util};
+
+#[derive(Debug, Encode)]
+pub struct Block {
+    prev_hash: Hash,
+    content: String,
+    timestamp: u64,
+}
+
+impl Block {
+    pub fn new(prev_hash: Hash, content: String) -> Self {
+        Self {
+            prev_hash: prev_hash,
+            content: content,
+            timestamp: util::now_secs(),
+        }
+    }
+
+    pub fn prev_hash(&self) -> Hash {
+        self.prev_hash
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+
+    pub fn timestamp(&self) -> u64 {
+        self.timestamp
+    }
+
+    pub fn hash(&self) -> Hash {
+        let bytes = bincode::encode_to_vec(self, config::standard()).unwrap();
+        Hash::hash(&bytes)
+    }
+}
+
+pub struct HashChain {
+    blocks: Vec<Block>,
+}
+
+impl HashChain {
+    pub fn new() -> Self {
+        Self { blocks: vec![] }
+    }
+
+    pub fn blocks(&self) -> &[Block] {
+        &self.blocks
+    }
+
+    pub fn add_block(&mut self, block: Block) {
+        self.blocks.push(block);
+    }
+}
